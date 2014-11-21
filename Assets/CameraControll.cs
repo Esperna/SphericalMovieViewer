7fb3f13f46d94bfad3bd5e35302bfe0010262f95
Takuya Mizuhara <z8x8c.zeolite.m@gmail.com>
2014-11-22 00:10:47 +0900

    Add texture change script

---
+++ Assets/CameraControll.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour {
+
+	void Update () {
+		transform.Rotate(
+			Vector3.right * 
+			Time.deltaTime);
+	}
+}
---
+++ Assets/TextureChange.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureChange : MonoBehaviour {
+	
+	public Texture image;
+	public Texture image2;
+	public Texture image3;
+	private int index = 0;
+	
+	// Use this for initialization
+	void Start () {
+		//gameObject取得
+		renderer.material = renderer.materials[index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetMouseButtonDown(2))
+		//{
+		index++;
+		if (index > 3 - 1) {
+			index = 0;
+		}
+		switch (index) {
+		case 0:
+			renderer.material.mainTexture = image;
+			break;
+		case 1:
+			renderer.material.mainTexture = image2;
+			break;
+		case 2:
+			renderer.material.mainTexture = image3;
+			break;
+			
+		}
+		
+		//renderer.material = renderer.materials[index];
+		//renderer.material.mainTexture = 
+		
+		Debug.Log (index);
+	}
+	//}
+}
