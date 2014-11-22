f878eaf1f14a0506bb56488b81bda7a694f9f257
ntoshi <anagosuki@live.jp>
2014-11-22 21:12:15 +0900

    Change for Oculus

--- Assets/GyroCameraController.cs
+++ Assets/GyroCameraController.cs
@@ -7,19 +7,17 @@ public class GyroCameraController : MonoBehaviour {
 	{
 		Quaternion gyro = Input.gyro.attitude;
 		this.transform.localRotation = Quaternion.Euler (90, 0, 0) * (new Quaternion (-gyro.x, -gyro.y, gyro.z, gyro.w));
-
-		const float dive_trigger_down_thresh = -0.1f;
-		const float dive_trigger_up_thresh = 0.3f;
+		
 		Debug.Log (Input.acceleration.x);
 
 		switch (GameObject.Find ("Sherer100").GetComponent<textureRenew> ().divingStatus) {
 		case DivingStatus.InitialLoop:
-			if (Input.acceleration.x < dive_trigger_down_thresh) {
+			if (Input.GetKeyDown(KeyCode.S)) {
 				GameObject.Find("Sherer100").GetComponent<textureRenew>().divingStatus = DivingStatus.GoingDown;
 			}
 			break;
 		case DivingStatus.EndLoop:
-			if(Input.acceleration.x > dive_trigger_up_thresh){
+			if(Input.GetKeyDown(KeyCode.W)){
 				GameObject.Find("Sherer100").GetComponent<textureRenew>().divingStatus = DivingStatus.GoingUp;
 			}
 			break;
