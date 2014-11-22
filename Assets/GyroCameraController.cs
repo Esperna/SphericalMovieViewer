3157967149217b77653c9b9c70da8f27e7187c72
esper0328 <esper0328@gmail.com>
2014-11-22 20:07:37 +0900

    neglect setting diving status when going down or up

--- Assets/GyroCameraController.cs
+++ Assets/GyroCameraController.cs
@@ -8,14 +8,25 @@ public class GyroCameraController : MonoBehaviour {
 		Quaternion gyro = Input.gyro.attitude;
 		this.transform.localRotation = Quaternion.Euler (90, 0, 0) * (new Quaternion (-gyro.x, -gyro.y, gyro.z, gyro.w));
 
-		const float dive_trigger_down_thresh = -0.05f;
-		const float dive_trigger_up_thresh = 0.07f;
+		const float dive_trigger_down_thresh = -0.1f;
+		const float dive_trigger_up_thresh = 0.3f;
 		Debug.Log (Input.acceleration.x);
 
-		if (Input.acceleration.x < dive_trigger_down_thresh) {
-			GameObject.Find("Sherer100").GetComponent<textureRenew>().divingStatus = DivingStatus.GoingDown;
-		} else {
-			GameObject.Find("Sherer100").GetComponent<textureRenew>().divingStatus = DivingStatus.GoingUp;
+		switch (GameObject.Find ("Sherer100").GetComponent<textureRenew> ().divingStatus) {
+		case DivingStatus.InitialLoop:
+			if (Input.acceleration.x < dive_trigger_down_thresh) {
+				GameObject.Find("Sherer100").GetComponent<textureRenew>().divingStatus = DivingStatus.GoingDown;
+			}
+			break;
+		case DivingStatus.EndLoop:
+			if(Input.acceleration.x > dive_trigger_up_thresh){
+				GameObject.Find("Sherer100").GetComponent<textureRenew>().divingStatus = DivingStatus.GoingUp;
+			}
+			break;
+		case DivingStatus.GoingDown:
+		case DivingStatus.GoingUp:
+		default:
+			break;
 		}
 	}
 }
